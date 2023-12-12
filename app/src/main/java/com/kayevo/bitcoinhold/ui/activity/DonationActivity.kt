cad651e64339a8db63495f8cb42bfaa2f55efc44
Kayevo <kayevo@protonmail.com>
2023-12-12 16:29:02 -0300

    feat: move sensitive data to environment variables.

--- app/src/main/java/com/kayevo/bitcoinhold/ui/activity/DonationActivity.kt
+++ app/src/main/java/com/kayevo/bitcoinhold/ui/activity/DonationActivity.kt
@@ -4,46 +4,39 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kayevo.bitcoinhold.databinding.ActivityDonationBinding
+import com.kayevo.bitcoinhold.BuildConfig
 
 class DonationActivity : AppCompatActivity() {
     private lateinit var donationView: ActivityDonationBinding
     private lateinit var clipboardManager: ClipboardManager
+    private val lNBitcoinAddress: String = BuildConfig.BITCOIN_LN_ADDRESS
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         donationView = ActivityDonationBinding.inflate(layoutInflater)
         setContentView(donationView.root)
+        setBitcoinAddress()
         setListeners()
+    }
+
+    private fun setBitcoinAddress(){
+        donationView.txtBitcoinAddress.text = BuildConfig.BITCOIN_ADDRESS
     }
 
     private fun setListeners() {
         with(donationView) {
             btnCopyBitcoinAddress.setOnClickListener {
-                copyText(txtBitcoinAddress)
+                copyText(txtBitcoinAddress.text.toString())
             }
             btnCopyLNAddress.setOnClickListener {
-                copyText("kayevo@zbd.gg")
+                copyText(lNBitcoinAddress)
             }
             btnTurnBack.setOnClickListener {
                 finish()
             }
-        }
-    }
-
-    private fun copyText(editText: TextView) {
-        val text = editText.text.toString()
-        if (text.isNotEmpty()) {
-            clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            val clipData = ClipData.newPlainText("BITCOIN_ADDRESS", text)
-            clipboardManager.setPrimaryClip(clipData)
-            showMessage("Copiado")
-        } else {
-            showMessage("Texto vazio")
         }
     }
 
--- app/src/main/java/com/kayevo/bitcoinhold/ui/activity/SupportActivity.kt
+++ app/src/main/java/com/kayevo/bitcoinhold/ui/activity/SupportActivity.kt
@@ -2,9 +2,8 @@ package com.kayevo.bitcoinhold.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kayevo.bitcoinhold.R
-import com.kayevo.bitcoinhold.databinding.ActivitySettingsBinding
 import com.kayevo.bitcoinhold.databinding.ActivitySupportBinding
+import com.kayevo.bitcoinhold.BuildConfig
 
 class SupportActivity : AppCompatActivity() {
     private lateinit var supportView: ActivitySupportBinding
@@ -13,8 +12,12 @@ class SupportActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportView = ActivitySupportBinding.inflate(layoutInflater)
         setContentView(supportView.root)
-
+        setSupportEmail()
         setListeners()
+    }
+
+    private fun setSupportEmail(){
+        supportView.txtSupportEmail.text = BuildConfig.SUPPORT_EMAIL
     }
 
     private fun setListeners() {
